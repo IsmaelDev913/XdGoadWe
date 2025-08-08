db6e265471272803c8405ec52563b3637b3e7184
IsmaelDev913 <josewwwjuan@gmail.com>
2025-08-08 12:20:12 -0600

    Update 11-group.js

--- CuervoOFC/11-group.js
+++ CuervoOFC/11-group.js
@@ -6,27 +6,27 @@ const groupHandler = async ({ bot, reply, args, m }) => {
   try {
   const metadata = await bot.groupMetadata(jid).catch(() => null);
   if (!metadata) {
-  return await bot.sendMessage(jid, { text: "> ❌ команда доступна только в группах." }, { contextInfo: { externalAdReply: { title: "Юмэко Бот", body: "работает на русской мафуа", mediaType: 1, previewType: 'PHOTO', thumbnailUrl: 'https://files.catbox.moe/651gmb.jpg', renderLargerThumbnail: true, sourceUrl: 'https://dash.cuervo-host.xyz' } }, }, { quoted: m });
+  return await reply("> ❌ команда доступна только в группах.");
   }
   const sender = m?.key?.participant || m?.key?.remoteJid || '';
   const admins = metadata.participants.filter(p => p.admin).map(p => p.id);
   const isAdmin = admins.includes(sender);
   if (!isAdmin) {
-  return await bot.sendMessage(jid, { text: "> ❌ Только администраторы могут использовать команду." }, { contextInfo: { externalAdReply: { title: "Юмэко Бот", body: "работает на русской мафуа", mediaType: 1, previewType: 'PHOTO', thumbnailUrl: 'https://files.catbox.moe/651gmb.jpg', renderLargerThumbnail: true, sourceUrl: 'https://dash.cuervo-host.xyz' } }, }, { quoted: m });
+  return await reply("> ❌ Только администраторы могут использовать команду.");
   }
   const action = (args[0] || "").toLowerCase();
   if (action === "открытая") {
   await bot.groupSettingUpdate(jid, "not_announcement");
-  await bot.sendMessage(jid, { text: "> 🔓 группа была *открытая*." }, { contextInfo: { externalAdReply: { title: "Юмэко Бот", body: "работает на русской мафуа", mediaType: 1, previewType: 'PHOTO', thumbnailUrl: 'https://files.catbox.moe/651gmb.jpg', renderLargerThumbnail: true, sourceUrl: 'https://dash.cuervo-host.xyz' } }, }, { quoted: m });
+  await reply("> 🔓 группа была *открытая*.");
   } else if (action === "закрытая") {
   await bot.groupSettingUpdate(jid, "announcement");
-  await bot.sendMessage(jid, { text: "> 🔒 группа была *закрытая*." }, { contextInfo: { externalAdReply: { title: "Юмэко Бот", body: "работает на русской мафуа", mediaType: 1, previewType: 'PHOTO', thumbnailUrl: 'https://files.catbox.moe/651gmb.jpg', renderLargerThumbnail: true, sourceUrl: 'https://dash.cuervo-host.xyz' } }, }, { quoted: m });
+  await reply("> 🔒 группа была *закрытая*.");
   } else {
-  await bot.sendMessage(jid, { text: ">❗ использование команды:\n.группа открытая\n.группа закрытая" }, { contextInfo: { externalAdReply: { title: "Юмэко Бот", body: "работает на русской мафуа", mediaType: 1, previewType: 'PHOTO', thumbnailUrl: 'https://files.catbox.moe/651gmb.jpg', renderLargerThumbnail: true, sourceUrl: 'https://dash.cuervo-host.xyz' } }, }, { quoted: m });
+  await reply(">❗ использование команды:\n.группа открытая\n.группа закрытая");
   }
   } catch (e) {
   console.error("Error en /group:", e);
-  await bot.sendMessage(jid, { text: "> ❌ Произошла ошибка при выполнении команды." }, { contextInfo: { externalAdReply: { title: "Юмэко Бот", body: "работает на русской мафуа", mediaType: 1, previewType: 'PHOTO', thumbnailUrl: 'https://files.catbox.moe/651gmb.jpg', renderLargerThumbnail: true, sourceUrl: 'https://dash.cuervo-host.xyz' } }, }, { quoted: m });
+  await reply("> ❌ Произошла ошибка при выполнении команды.");
   }
 
 }
