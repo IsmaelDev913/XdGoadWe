90dda96831b966516829ab361799cf563664f995
IsmaelDev913 <josewwwjuan@gmail.com>
2025-08-08 12:13:16 -0600

    Update 12-tag.js

--- CuervoOFC/12-tag.js
+++ CuervoOFC/12-tag.js
@@ -7,42 +7,25 @@ const tangHandler = async ({ reply, bot, args, m }) => {
   await bot.sendMessage(jid, { react: { text: "⏱️", key: m.key } });
   try {
   if (!jid.endsWith("@g.us")) {
-  await sock.sendMessage(jid, { 
-    contextInfo: { 
-    externalAdReply: { 
-    title: "Юмэко Бот", 
-    body: "работает на русской мафуа", 
-    mediaType: 1, 
-    renderLargerThumbnail: true, 
-    previewType: 'PHOTO', 
-    thumbnailUrl: 'https://files.catbox.moe/651gmb.jpg', 
-    mediaUrl: 'https://dash.cuervo-host.xyz''
-    sourceUrl: 'https://dash.cuervo-host.xyz' } }, 
-    image: { url: 'https://files.catbox.moe/651gmb.jpg' }
-    text: "❌ Эта команда работает только в группах."
-    },
-    { quoted: m });
+  await reply("❌ Эта команда работает только в группах.");
   return;
   }
   const groupMetadata = await sock.groupMetadata(jid);
   const admins = groupMetadata.participants.filter(p => p.admin !== null).map(p => p.id);
   // Verificar si el que manda es admin
   if (!admins.includes(m.key.participant || m.participant)) {
-  await bot.sendMessage(jid, { text: "❌ Эту команду могут использовать только администраторы." }, { contextInfo: { externalAdReply: { title: "Юмэко Бот", body: "работает на русской мафуа", mediaType: 1, previewType: 'PHOTO', thumbnailUrl: 'https://files.catbox.moe/651gmb.jpg', renderLargerThumbnail: true, sourceUrl: 'https://dash.cuervo-host.xyz' } }, }, { quoted: m });
+  await reply("❌ Эту команду могут использовать только администраторы.");
   return;
   }
   if (args.length === 0) {
-  await bot.sendMessage(jid, { text: "❗ использование команды: .ярлык [сообщение]" }, { contextInfo: { externalAdReply: { title: "Юмэко Бот", body: "работает на русской мафуа", mediaType: 1, previewType: 'PHOTO', thumbnailUrl: 'https://files.catbox.moe/651gmb.jpg', renderLargerThumbnail: true, sourceUrl: 'https://dash.cuervo-host.xyz' } }, }, { quoted: m });
+  await reply("❗ использование команды: .ярлык [сообщение]" }, { contextInfo: { externalAdReply: { title: "Юмэко Бот", body: "работает на русской мафуа", mediaType: 1, previewType: 'PHOTO', thumbnailUrl: 'https://files.catbox.moe/651gmb.jpg', renderLargerThumbnail: true, sourceUrl: 'https://dash.cuervo-host.xyz' } }, }, { quoted: m });
   return;
   }
   const texto = args.join(" ");
   await bot.sendMessage(jid, { text: texto, mentions: groupMetadata.participants.map(p => p.id) });
   } catch (error) {
   console.error(error);
-  await bot.sendMessage(jid, { contextInfo: { externalAdReply: { title: "Юмэко Бот", body: "работает на русской мафуа", mediaType: 1, previewType: 'PHOTO', thumbnailUrl: 'https://files.catbox.moe/651gmb.jpg', renderLargerThumbnail: true, sourceUrl: 'https://dash.cuervo-host.xyz' } }, 
-   imagen: 
-   text: "❌ ошибка в работе команды." 
-  }, { quoted: m });
+  await reply("❌ ошибка в работе команды.");
   }
 };
 export default tagHandler;
